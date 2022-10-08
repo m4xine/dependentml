1e9fdf13f85e2ca70a80b526cd5e1776edea1c24
m4xine <maxined@pm.me>
2022-10-08 17:16:04 -0400

    Add `mkSrcSpan` and `SrcAnn`

--- app/Source.hs
+++ app/Source.hs
@@ -1,7 +1,15 @@
 module Source 
-  ( Source(..)
+  ( SourcePos
+  , Pos
+  , Source(..)
   , readSource
+  , SrcSpan(..)
+  , mkSrcSpan
+  , SrcAnn(..)
   ) where
+
+import  Text.Megaparsec    (SourcePos(SourcePos), Pos)
+import  Control.Exception  (assert)
 
 -- | Representation of a source file.
 data Source = MkSource
@@ -17,3 +25,29 @@ readSource filePath = do
     { sourcePath = filePath
     , sourceContent = fileContent
     }
+
+-- | A specific portion within a source file.
+data SrcSpan = MkSrcSpan
+  { srcName   :: FilePath
+  , srcBegin  :: !(Pos, Pos) -- ^ (line, col)
+  , srcEnd    :: !(Pos, Pos) -- ^ (line, col)
+  }
+
+-- | Constructs a 'SrcSpan' from two 'SourcePos'.
+-- 
+-- This will fail if the source names of the provided 'SourcePos'
+-- are not the same or if the beginning 'SourcePos' is greater than
+-- the ending 'SourcePos'.
+mkSrcSpan 
+  :: SourcePos -- ^ Beginning
+  -> SourcePos -- ^ Ending 
+  -> SrcSpan
+mkSrcSpan (SourcePos p bl bc) (SourcePos p' el ec) =
+  assert (p == p' && ((bl == el && bc <= ec) || (bl < el))) 
+    $ MkSrcSpan p (bl, bc) (el, ec)  
+
+-- | A 'SrcAnn' wraps a 'SrcSpan' with an extra annotation type.
+data SrcAnn a = MkSrcAnn
+  { ann     :: a
+  , annSrc  :: SrcSpan 
+  }
